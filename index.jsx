3068fae1f17f6697490d6ac724fd1302120747d1
Raphael Hemme <hemme.raphael@gmail.com>
2020-09-16 18:19:02 +0200

    Delete the script file with the wrong suffix

--- index.jsx
+++
@@ -1,8 +0,0 @@
-import "./style.css";
-
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
