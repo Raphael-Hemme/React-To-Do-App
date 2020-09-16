725a188369fc4d4a1254b27b80b66b0efa9b6f5a
Raphael Hemme <hemme.raphael@gmail.com>
2020-09-16 17:02:58 +0200

    Integrate both designs by Magima and tgchris0

---
+++ index.js
@@ -0,0 +1,8 @@
+import "./style.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
